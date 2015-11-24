834f93be90aa919a66b9165de3baedc39d39d5c1
Luis <delapenl@gmail.com>
2015-11-23 20:20:00 -0700

    started to make front end routes and views

---
+++ src/client/js/routes.js
@@ -0,0 +1,27 @@
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html'
+    })
+    .when('/products', {
+      templateUrl: 'views/products.html'
+    })
+    .when('/add-product', {
+      templateUrl: 'views/add-product.html'
+    })
+    .when('/register', {
+      templateUrl: 'views/register.html'
+    })
+    .when('/customer-login', {
+      templateUrl: 'views/customer-login.html'
+    })
+    .when('/admin-login', {
+      templateUrl: 'views/admin-login.html'
+    })
+    .when('/customer-service', {
+      templateUrl: 'views/customer-service.html'
+    })
+    .when('checkout', {
+      templateUrl: 'views/checkout.html'
+    });
+});
