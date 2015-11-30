6e5cb97b162613581f51dd8e7c3d16654065788b
Luis <delapenl@gmail.com>
2015-11-30 13:20:00 -0700

    created view for specific style of bt

--- src/client/js/routes.js
+++ src/client/js/routes.js
@@ -26,5 +26,28 @@ app.config(function($routeProvider) {
     })
     .when('/drop-in-style-ball-transfer', {
       templateUrl: 'views/drop-in-style-ball-transfer.html'
+    })
+    .when('/flange-mounted-ball-transfer', {
+      templateUrl: 'views/flange-mounted-ball-transfer.html'
+    })
+    .when('/flangeless-ball-transfer', {
+      templateUrl: 'views/flangeless-ball-transfer.html'
+    })
+    .when('/flying-saucer-ball-transfer', {
+      templateUrl: 'views/flying-saucer-ball-transfer.html'
+    })
+    .when('/pipe-mounted-ball-transfer', {
+      templateUrl: 'views/pipe-mounted-ball-transfer.html'
+    })
+    .when('/stud-mounted-ball-transfer', {
+      templateUrl: 'views/stud-mounted-ball-transfer.html'
     });
+
 });
+
+
+
+
+
+
+
