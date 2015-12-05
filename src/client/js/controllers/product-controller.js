a66f9223484189915714b9e31071c66d8b9ad31e
Luis <delapenl@gmail.com>
2015-12-05 13:40:31 -0700

    load capacity filter is good

--- src/client/js/controllers/product-controller.js
+++ src/client/js/controllers/product-controller.js
@@ -2,6 +2,50 @@ app.controller('productCtrl', ['$scope','httpService', '$location', '$uibModal',
 
   console.log('it is working');
   $scope.orderHistory = cartService.getCart();
+  $scope.types = [
+    'Flange-Mounted Ball Transfer',
+    'Flangeless',
+    'Stud Mounted',
+    'Stud Mounted Machine',
+    'Stud Mounted Machine',
+    'Stud Mounted Heavy Duty',
+    'Pipe Mounted','Drop In',
+    'Drop In Heavy Duty',
+    'Flying Saucer'
+  ];
+  $scope.prices = [
+    {
+      label: '$0-$20',
+      range: [0, 20]
+    },
+    {
+      label: '$20-$40',
+      range: [20, 40]
+    },
+    {
+      label: '$40-$60',
+      range: [40, 60]
+    },
+    {
+      label: '$60',
+      range: [60, 9999999999]
+    }
+  ];
+  $scope.loadCapacity = [
+    '10',
+    '17',
+    '20',
+    '33',
+    '35',
+    '40',
+    '75',
+    '100',
+    '200',
+    '125',
+    '250',
+    '500',
+    '750'
+  ];
 
   $scope.addProduct = function() {
     $scope.products = httpService.addProduct($scope.product);
@@ -51,6 +95,29 @@ app.controller('productCtrl', ['$scope','httpService', '$location', '$uibModal',
               backdrop: 'static'
             });
           };
+
+  $scope.filter = function(filterCategory, attribute) {
+    console.log(filterCategory, attribute)
+    var temp = [];
+    for(i=0; i< $scope.products.length; i++){
+      if($scope.products[i][filterCategory] === attribute){
+        temp.push($scope.products[i]);
+        }
+      }
+    $scope.products = temp;
+  };
+
+    $scope.priceFilter = function(stringValue) {
+      var range = $scope.prices.filter(function(obj){
+        return obj.label === stringValue;
+      })[0].range;
+      console.log(range)
+      var temp = $scope.products.filter(function(obj){
+        return obj.price > range[0] && obj.price <= range[1];
+      });
+      $scope.products = temp;
+      console.log($scope.products)
+  };
 
 
 
