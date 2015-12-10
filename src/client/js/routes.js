14bff0b344e1620155150d3bab548f2e966ec455
Luis <delapenl@gmail.com>
2015-12-09 17:37:08 -0700

    changed style! ZOe is awesome!

--- src/client/js/routes.js
+++ src/client/js/routes.js
@@ -69,6 +69,9 @@ app.config(function($routeProvider) {
     .when('/credit-card-error', {
       templateUrl: 'views/credit-card-error.html',
       access: {restricted: false}
+    })
+    .when('/test', {
+      templateUrl: 'views/test.html'
     });
 
 });
